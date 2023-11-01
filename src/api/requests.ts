799d51e09d739d00e4de5ac5b33b67c6dacdebf5
Albert <albertsoares97@gmail.com>
2023-11-01 04:47:13 -0300

    feat/add-requests

---
+++ src/api/requests.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const requestHerosList = async () => {
+  try {
+    const response = await axios.get(
+      "http://homologacao3.azapfy.com.br/api/ps/metahumans"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("***erro na requisição", error);
+    throw new Error();
+  }
+};
